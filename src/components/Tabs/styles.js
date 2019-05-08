969126f5d8644eec2646658d813da0219faf6d91
Luis Fernando <lsh.fernandors@gmail.com>
2019-05-08 00:15:02 -0300

    adding menu buttons

--- src/components/Tabs/styles.js
+++ src/components/Tabs/styles.js
@@ -8,10 +8,20 @@ export const Container = styled.View`
 export const TabsContainer = styled.ScrollView.attrs({
   horizontal: true,
   contentContainerStyle: { paddingLeft: 10, paddingRight: 20 },
-  showsHorizontalSctrollIndicator: false,
+  showsHorizontalScrollIndicator: false,
 })``;
 
 export const TabItem = styled.View`
   width: 100px;
   height: 100px;
+  background: rgba(255, 255, 255, 0.2);
+  border-radius: 3px;
+  margin-left: 10px;
+  padding: 10px;
+  justify-content: space-between;
+`;
+
+export const TabText = styled.Text`
+  font-size: 13px;
+  color: #FFF;
 `;
