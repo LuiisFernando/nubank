f05e3b9de524b3216ad55abdfc6df171a2a15a50
Luis Fernando <lsh.fernandors@gmail.com>
2019-05-07 22:06:26 -0300

    adding Header

---
+++ src/components/Tabs/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  height: 100px;
+  margin-top: 20px;
+`;
+
+export const TabsContainer = styled.ScrollView.attrs({
+  horizontal: true,
+  contentContainerStyle: { paddingLeft: 10, paddingRight: 20 },
+  showsHorizontalSctrollIndicator: false,
+})``;
+
+export const TabItem = styled.View`
+  width: 100px;
+  height: 100px;
+`;
